f817773212c29866cca7bbb8bcb0f4cbb85fbc3c
Aryan1274 <aryan407012@gmail.com>
2026-05-09 22:09:13 +0530

    cleanup: remove redundant settings files

--- server/models/Settings.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const settingsSchema = new mongoose.Schema({
-  key: { type: String, required: true, unique: true }, // e.g., 'payment_methods'
-  value: { type: mongoose.Schema.Types.Mixed, required: true } // e.g., { cod: true, online: true, whatsapp: true }
-}, { timestamps: true });
-
-module.exports = mongoose.model('Settings', settingsSchema);
